c01c48c4094c8881e481cb29116aaa0587639ba1
bobchin <bobchin.ryu@gmail.com>
2018-03-08 16:02:10 +0900

    4.6.5途中

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -5,9 +5,8 @@ def to_hex(r, g, b)
 end
 
 def to_ints(hex)
-    r = hex[1..2]
-    g = hex[3..4]
-    b = hex[5..6]
+    # r, g, b = hex.scan(/\w\w/)
+    r, g, b = hex[1..2], hex[3..4], hex[5..6]
     
     [r, g, b].map do |s|
         s.hex
