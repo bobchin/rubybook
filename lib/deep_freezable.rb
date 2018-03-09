0c12d48b8cd8536654d9a913b5a95f5313ba5a66
bobchin <bobchin.ryu@gmail.com>
2018-03-09 15:58:22 +0900

    8.4.3を追加

--- lib/deep_freezable.rb
+++ lib/deep_freezable.rb
@@ -1,6 +1,13 @@
 module DeepFreezable
    
     def deep_freeze(array_of_hash)
+        # 配列の各要素をfreeze
+        array_of_hash.each do |element|
+            element.freeze
+        end
+        
+        # 配列自身をfreeze
+        array_of_hash.freeze
     end
 
 end
