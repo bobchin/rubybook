bf7a2241133c472384f1c69154c666bbdf81e5cd
bobchin <bobchin.ryu@gmail.com>
2018-03-11 17:34:47 +0900

    10.4.5を追加

--- lib/effects.rb
+++ lib/effects.rb
@@ -1,5 +1,13 @@
 module Effects
     def self.reverse
-        # 実装はあとで
+        ->(words) do
+            words.split(' ').map(&:reverse).join(' ')
+        end
+    end
+
+    def self.echo(rate)
+        ->(words) do
+            words.chars.map{|c| c == ' '? c: c * rate}.join
+        end
     end
 end
